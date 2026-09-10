ed3f07c7ad323661008e2dd436d564076620b42b
tau <36781951+tndd@users.noreply.github.com>
2026-09-11 04:40:44 +0900

    Add package initializer

---
+++ src/religious_protocols/__init__.py
@@ -0,0 +1,5 @@
+"""Executable research models for the Religious Protocols project."""
+
+from .scoring import log_score_gain, update_magnitude_bernoulli
+
+__all__ = ["log_score_gain", "update_magnitude_bernoulli"]
